d88d14e9fca93e9433ff0b95065f8530da9c8332
uzdevid <uzdevid@gmail.com>
2023-09-14 16:17:32 +0500

    Исправил ошибки

--- controllers/RoleController.php
+++ controllers/RoleController.php
@@ -3,7 +3,9 @@
 namespace uzdevid\dashboard\access\control\controllers;
 
 use uzdevid\dashboard\access\control\filters\DashboardAccessControl;
+use uzdevid\dashboard\access\control\models\Action;
 use uzdevid\dashboard\access\control\models\Role;
+use uzdevid\dashboard\access\control\models\service\ActionService;
 use uzdevid\dashboard\base\helpers\Url;
 use uzdevid\dashboard\base\web\Controller;
 use uzdevid\dashboard\widgets\ModalPage\ModalPage;
@@ -14,6 +16,7 @@ use yii\data\ActiveDataProvider;
 use yii\db\StaleObjectException;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
+use yii\web\BadRequestHttpException;
 use yii\web\NotFoundHttpException;
 use yii\web\Response;
 
@@ -40,6 +43,10 @@ class RoleController extends Controller {
         $behaviors['verbs'] = [
             'class' => VerbFilter::class,
             'actions' => [
+                'index' => ['GET'],
+                'create' => ['GET', 'POST'],
+                'update' => ['GET', 'POST'],
+                'view' => ['GET'],
                 'delete' => ['POST'],
             ],
         ];
@@ -152,6 +159,31 @@ class RoleController extends Controller {
     }
 
     /**
+     * @param int $id
+     *
+     * @return array|string
+     * @throws NotFoundHttpException
+     */
+    public function actionPermissions(int $id): array|string {
+        $model = $this->findModel($id);
+
+        $actions = Action::find()->orderBy(['action' => SORT_ASC])->all();
+
+        if (!$this->request->isAjax) {
+            return $this->render('permissions', compact('model', 'actions'));
+        }
+
+        return [
+            'success' => true,
+            'modal' => ModalPage::options(true, ModalPageOptions::SIZE_FULLSCREEN),
+            'body' => [
+                'title' => ModalPage::title($model->name, '<i class="bi bi-shield"></i>'),
+                'view' => $this->renderAjax('modal/permissions', compact('model', 'actions'))
+            ]
+        ];
+    }
+
+    /**
      * @param int $id ID
      *
      * @return Response
@@ -162,6 +194,13 @@ class RoleController extends Controller {
         $this->findModel($id)->delete();
 
         return $this->redirect(Url::to(['index']));
+    }
+
+    /**
+     * @throws NotFoundHttpException
+     */
+    public function actionSavePermission(): array {
+        
     }
 
     /**
